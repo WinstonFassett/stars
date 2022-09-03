6490ae2fc40de3a1212c3c5d1fb029adf5c79436
Winston Fassett <winston.fassett@gmail.com>
2022-09-03 18:59:12 -0500

    reorder CSV columns, add language, html_url, fork

--- etl/stars-to-csv.sh
+++ etl/stars-to-csv.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-echo "name, starred_at, full_name, owner_name, owner_avatar_url, description, homepage, stargazers_count, forks_count, open_issues_count, license_name, topics, created_at, updated_at, pushed_at, archived"
+echo "full_name, description, language, topics, license_name, stargazers_count, forks_count, homepage, html_url, open_issues_count, name, owner_name, owner_avatar_url, created_at, starred_at, updated_at, pushed_at, fork, archived"
 
 jq -r '.[] 
 | {starred_at} + .repo 
 | . + { 
-    license_name: .license.name,
     owner_name: .owner.login, 
     owner_avatar_url: .owner.avatar_url,
+    license_name: .license.name,
+    topics: (.topics|join(", "))
 }
-| { name, starred_at, full_name, owner_name, owner_avatar_url, description, homepage, stargazers_count, forks_count, open_issues_count, license_name, topics, created_at, updated_at, pushed_at, archived }
-| [ .name, .starred_at, .full_name, .owner_name, .owner_avatar_url, .description, .homepage, .stargazers_count, .forks_count, .open_issues_count, .license_name, (.topics|join(", ")), .created_at, .updated_at, .pushed_at, .archived ]
+| { full_name, description, language, topics, license_name, stargazers_count, forks_count, homepage, html_url, open_issues_count, name, owner_name, owner_avatar_url, created_at, starred_at, updated_at, pushed_at, fork, archived }
+| [ .full_name, .description, .language, .topics, .license_name, .stargazers_count, .forks_count, .homepage, .html_url, .open_issues_count, .name, .owner_name, .owner_avatar_url, .created_at, .starred_at, .updated_at, .pushed_at, .fork, .archived ]
 | @csv
 ' $1
